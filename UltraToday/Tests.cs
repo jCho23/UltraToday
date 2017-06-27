3cbbc0e0f3d308566d82a05d913045fb8cf1403d
JuneCho23 <junecho.ucb@gmail.com>
2017-06-27 13:52:53 -0700

    Test we went over today
    Test we went over today

---
+++ UltraToday/Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UltraToday
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+            app.Screenshot("App Launched");
+        }
+
+        [Test]
+        public void Repl()
+        {
+            app.Repl();
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+			app.Tap("MyEntry");
+			app.ClearText();
+			app.EnterText("ILoveXamarin");
+			app.DismissKeyboard();
+        }
+    }
+}
